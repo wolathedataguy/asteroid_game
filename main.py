28b91e3ac2ac338208b36450f1bb1c39075f3d20
wolathedataguy <adekogs85@gmail.com>
2026-03-31 22:45:59 +0100

    add shooting mechanics

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -16,10 +17,12 @@ def main():
     drawable = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     
     asteroid_field = AsteroidField()
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
--- player.py
+++ player.py
@@ -1,9 +1,17 @@
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, LINE_WIDTH, PLAYER_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_TURN_SPEED,
+    LINE_WIDTH,
+    PLAYER_SPEED,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SHOOT_COOLDOWN_SECONDS,
+)
 from circleshape import CircleShape
 import pygame
+from shot import Shot
+
 
 class Player(CircleShape):
-    
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         if hasattr(self, "containers"):
@@ -11,10 +19,13 @@ class Player(CircleShape):
             for group in self.containers:
                 group.add(self)
         self.rotation = 0
+        self.shot_cooldown = 0
+
     # in the Player class
-    
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -22,23 +33,38 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-    def rotate(self,  dt):
+
+    def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-        
+
     def update(self, dt):
+        if self.shot_cooldown > 0:
+            self.shot_cooldown -= dt
         keys = pygame.key.get_pressed()
-        
+
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
-            self.rotate(dt) 
+            self.rotate(dt)
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-    
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
     def move(self, dt):
-        unit_vector =  pygame.Vector2(0, 1)
+        unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        if self.shot_cooldown > 0:
+            return
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        return shot
+    
+    
